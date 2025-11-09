e08beeb7d3d790cb183989b9bfc49757346ce7a6
lilalr <gurennut@gmail.com>
2025-11-09 09:28:28 +0700

    membuat file tampil data

---
+++ app/src/main/java/com/example/questnavigasitugas_041/view/TampilData.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasitugas_041.view
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+
+

a799f6b047611c713a846cb3c8900acdc8b5ec10
lilalr <gurennut@gmail.com>
2025-11-09 09:28:43 +0700

    menambah data class

--- app/src/main/java/com/example/questnavigasitugas_041/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasitugas_041/view/TampilData.kt
@@ -5,6 +5,12 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-
+data class TampilData(
+    val namaLengkap: String = "",
+    val jenisKelamin: String = "",
+    val jabatan: String = "",
+    val departemen: String = "",
+    val status: String = ""
+)
 
 

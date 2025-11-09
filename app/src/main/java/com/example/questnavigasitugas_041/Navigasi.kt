d89ce73817d2bfc9790d7e4de3d6df0cf7b275c3
lilalr <gurennut@gmail.com>
2025-11-09 09:31:17 +0700

    menambah enum class utk mendefinisikan tiga halaman

--- app/src/main/java/com/example/questnavigasitugas_041/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_041/Navigasi.kt
@@ -1,4 +1,19 @@
 package com.example.questnavigasitugas_041
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class HalamanApp {
+    Welcome,
+    List,
+    Formulir
+}
+
 
 

19bfa4242293018210fa1a742fa8e733b9bac9b9
lilalr <gurennut@gmail.com>
2025-11-09 09:06:49 +0700

    membuat file formulir dan composable formulir

---
+++ app/src/main/java/com/example/questnavigasitugas_041/view/Formulir.kt
@@ -0,0 +1,29 @@
+package com.example.questnavigasitugas_041.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormulirScreen(
+    onSubmit: (TampilData) -> Unit,
+    onKembali: () -> Unit
+) {
+
